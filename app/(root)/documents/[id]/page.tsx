1b7250154137fb1a7d00282949396281227c209f
mkaidev <mkaidev88@gmail.com>
2024-07-20 00:26:54 +0700

    authentication - clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 
@@ -6,8 +8,14 @@ const Document = () => {
     <div>
       <Header>
         <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">document title</p>
+          <p className="document-title">Share</p>
         </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
